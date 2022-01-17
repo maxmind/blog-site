afc2b139d120c87d5afb7acc9e9afc57134746f9
Dallas Hogan <dhogan@maxmind.com>
2022-01-17 13:58:04 -0500

    Fixing pagination styling

--- src/templates/Post/Post.tsx
+++ src/templates/Post/Post.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 import Layout from '../../components/Layout/Layout';
 import { h1 as H1, hr as Hr, p as P } from '../../components/Mdx';
-import Pagination from '../../components/Pagination';
+import Pagination from '../../components/PaginationPost';
 import Tag from '../Home/Tag';
 import { IPostContext } from './query';
 
