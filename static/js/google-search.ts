6042c5778cd366ca4bfc6ca7251940df6f510e03
Dallas Hogan <dhogan@maxmind.com>
2022-06-13 13:29:28 -0400

    Draft working on search

---
+++ static/js/google-search.ts
@@ -0,0 +1,54 @@
+interface IItems {
+  cacheId: string;
+  htmlSnippet: string;
+  htmlTitle: string;
+  link: string;
+}
+
+interface IQuery {
+  count: number;
+  startIndex: number;
+  totalResults: string;
+}
+
+export interface ISearchResults {
+  items?: IItems[];
+  queries: {
+    nextPage?: IQuery[];
+    previousPage?: IQuery[];
+    request: IQuery[];
+  }
+  spelling?: {
+    correctedQuery: string;
+  }
+}
+
+const endpoint =
+  'https://www.googleapis.com/customsearch/v1/siterestrict?cx={cx}&key={key}';
+
+const cx = '5204c164979744d30';
+const key = 'AIzaSyAI4atAz3I5ujXCjoEXRvdwqcYn3AIsCA8';
+
+const url = endpoint
+  .replace('{cx}', cx)
+  .replace('{key}', key);
+
+
+const GoogleSearch =
+  async (query: string, startIndex: number | null): Promise<ISearchResults> => {
+    let requestUrl = `${url}&q=${query}`;
+
+    if (startIndex) {
+      requestUrl = requestUrl + `&start=${startIndex}`;
+    }
+
+    const response = await fetch(requestUrl);
+
+    if (!response.ok) {
+      throw new Error(`There was an error searching for ${query}`);
+    }
+
+    return await response.json() as ISearchResults;
+  };
+
+export default GoogleSearch;
