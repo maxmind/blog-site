9cf17ae3ae4a536748671f0f6b6c6fd943a26457
Gregory Oschwald <goschwald@maxmind.com>
2025-04-03 08:29:29 -0700

    Build future blog posts on preview site

--- build.sh
+++ build.sh
@@ -2,4 +2,10 @@
 
 set -eu
 
-hugo --gc --minify -b "$CF_PAGES_URL"
+extra_args=()
+
+if [[ ${PREVIEW:-} ]]; then
+  extra_args+=("--buildFuture")
+fi
+
+hugo --gc --minify -b "$CF_PAGES_URL" "${extra_args[@]}"
